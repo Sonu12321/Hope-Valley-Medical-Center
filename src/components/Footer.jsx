57de9c69010ed72265858e291d7fc2e4ddb3917d
Sonu Santhosh <sonusanthosh371@gmail.com>
2024-07-30 11:17:09 +0530

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaLocationArrow, FaPhone } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 
 const Footer = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const hours = [
     { id: 1, day: "Monday", time: "9:00 AM - 11:00 PM" },
     { id: 2, day: "Tuesday", time: "12:00 PM - 12:00 AM" },
@@ -13,69 +24,121 @@ const Footer = () => {
     { id: 6, day: "Saturday", time: "9:00 AM - 3:00 PM" },
   ];
 
+  const footerStyle = {
+    textAlign: "center",
+    padding: "20px 0",
+    backgroundColor: "#282c34",
+    color: "#fff",
+  };
+
+  const hrStyle = {
+    border: "1px solid #fff",
+  };
+
+  const contentStyle = {
+    display: "flex",
+    flexDirection: isMobile ? "column" : "row",
+    justifyContent: "space-around",
+    padding: "20px 0",
+  };
+
+  const sectionStyle = {
+    flex: 1,
+    margin: "10px 0",
+  };
+
+  const ulStyle = {
+    listStyleType: "none",
+    padding: 0,
+  };
+
+  const liStyle = {
+    margin: "5px 0",
+  };
+
+  const linkStyle = {
+    color: "#61dafb",
+    textDecoration: "none",
+  };
+
+  const hoursListStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    margin: "5px 0",
+  };
+
+  const iframeStyle = {
+    border: 0,
+    width: "100%",
+    height: "300px",
+    maxWidth: "600px",
+  };
+
   return (
     <>
-      <footer className="container" style={{textAlign:'center'}}>
-        <hr />
-        <div className="content">
-          <div>
-            <img src="/logoss.png" alt="logo" className="logo-img" />
-          </div>
-          <div>
+      <footer style={footerStyle}>
+        <hr style={hrStyle} />
+        <div style={contentStyle}>
+          <div style={sectionStyle}>
             <h4>Quick Links</h4>
-            <ul>
-              <Link to="/">Home</Link>
-              <Link to="/appointment">Appointment</Link>
-              <Link to="/about">About</Link>
+            <ul style={ulStyle}>
+              <li style={liStyle}>
+                <Link to="/" style={linkStyle}>
+                  Home
+                </Link>
+              </li>
+              <li style={liStyle}>
+                <Link to="/appointment" style={linkStyle}>
+                  Appointment
+                </Link>
+              </li>
+              <li style={liStyle}>
+                <Link to="/about" style={linkStyle}>
+                  About
+                </Link>
+              </li>
             </ul>
           </div>
-          <div >
+          <div style={sectionStyle}>
             <h4>Hours</h4>
-            <ul style={{alignItems:'center'}}>
+            <ul style={ulStyle}>
               {hours.map((element) => (
-                <li key={element.id}>
+                <li key={element.id} style={hoursListStyle}>
                   <span>{element.day}</span>
                   <span>{element.time}</span>
                 </li>
               ))}
             </ul>
           </div>
-          <div>
+          <div style={sectionStyle}>
             <h4>Contact</h4>
-            <div >
-              
-              <span style={{color:'#fff',justifyContent:'center'}}>
-                1012-45454 <br/>
-                0560-45641
-              </span>
+            <div style={{ margin: "10px 0" }}>
+              <FaPhone style={{ marginRight: "8px" }} />
+              <span>1012-45454</span>
+              <br />
+              <span>0560-45641</span>
             </div>
-            <div>
-              
-              <span style={{color:'#fff'}}>himinli@gmail.com</span>
+            <div style={{ margin: "10px 0" }}>
+              <MdEmail style={{ marginRight: "8px" }} />
+              <span>himinli@gmail.com</span>
             </div>
-            <div>
-              
-              <span style={{color:'#fff'}}>Kochi, India</span>
+            <div style={{ margin: "10px 0" }}>
+              <FaLocationArrow style={{ marginRight: "8px" }} />
+              <span>Kochi, India</span>
             </div>
           </div>
         </div>
-        <div
-          style={{
-            marginTop: "20px",
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.3422212412987!2d75.78041!3d10.85052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba65d3f0c000001!2sKochi%2C%20India!5e0!3m2!1sen!2sus!4v1626236422146!5m2!1sen!2sus"
-            width="600"
-            height="450"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+        <div>
+          <h4>Location</h4>
+          <div style={{ marginTop: "20px", display: "flex", justifyContent: "center" }}>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.3422212412987!2d75.78041!3d10.85052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba65d3f0c000001!2sKochi%2C%20India!5e0!3m2!1sen!2sus!4v1626236422146!5m2!1sen!2sus"
+              style={iframeStyle}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
         </div>
       </footer>
     </>
